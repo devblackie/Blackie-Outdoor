eb0647cc6583a8dca744a3699ac9f3787df115aa
devblackie <newtieblackie@gmail.com>
2022-06-27 20:14:38 +0300

    Added the licence, README FILE and Json extensions

--- Assets/index.js
+++ Assets/index.js
@@ -1,6 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-
-const btn = document.getElementsByClassName("btn")
 
 const login = document.getElementById("login")
 
@@ -10,7 +9,49 @@ function logInPrompt(){
 login.addEventListener("mouseover",logInPrompt)
 
 
-function toNext{
-    return ('Walk with us')
+
+
+
+function toNext(){
+    fetch ("https://devblackie.github.io/Data/db.json")
+     .then(response => response.json())
+      .then(data => data.event.forEach(element => {
+    console.log(element.image)
+    divCreator(element)
+  }))
 }
+
+const btn = document.getElementById("btn-more")
 btn.addEventListener("click",toNext)
+
+
+ 
+
+
+  let upcoming = document.getElementById('upcoming')
+  function divCreator(data){
+     
+      let div = document.createElement('div')
+      div.className="box"
+      upcoming.append(div)
+      let image = document.createElement('img')
+      image.src=data.image
+      div.append(image)
+      let divChild = document.createElement('div')
+      div.append(divChild)
+      divChild.className="content"
+      let h3 = document.createElement('h3')
+      h3=data.event_type
+      divChild.append(h3)
+      let location = document.createElement('p')
+      location=data.location
+      divChild.append(location)
+      let amount = document.createElement('h4')
+      amount=data.amount
+      divChild.append(amount)
+
+  }
+})
+
+
+
---
+++ README.md
@@ -0,0 +1,42 @@
+## Blackie-Outdoor
+
+## Author
+
+Newton Mboi
+
+## Description
+
+Online booking site for tours,hikes, team building services and fitness coaching.
+
+## Setup/Installation Requirements
+
+To acces this website you need to clone it using these steps
+
+* Open your terminal on you Ubuntu or Mac or commandprompt on windows
+* Change your current woking directory to the location where you want to clone the project.
+* Type $ git clone git@github.com:devblackie/Blackie-Outdoor.git
+* The command will clone the repository into the location folder selected.
+
+## Known Bugs
+
+* No noticeable bugs identified.
+
+## Technology Used
+
+* HTML
+* CSS
+* JavaScript
+
+## Support and Contact Details
+
+Incase you run into a problem, kindly contact  me on newtieblackie@gmail.com and also feel free to contribute to my code.
+
+## Live Site
+
+Go to <a src="">Live</a>
+
+## Licence
+
+This project is under the MIT license:
+
+## Copyright (c) 2022 Newton Mboi
